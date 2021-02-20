4393e6bdcc12370b8d24abdba5e27dae98abb28e
AlexanderTheGreat26 <kaegod@yandex.ru>
2021-02-20 22:21:26 +0300

    v1.3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cmath>
 #include <tuple>
-#include <cstdlib>
 #include <ctime>
 
-const long N = 1.0e5;
+const long N = 1.0e4;
 const double eps = 1.0 / N;
 const double pi = 3.14159265359;
 
@@ -16,14 +15,13 @@ double accuracy(double x) {
 }
 
 double fun(double rho, double m) {
-    return 1 / std::pow(pi, 2) / std::pow(rho, m);
+    return 1 / std::pow(rho, m);
 }
 
 DoubleTuple roulette() {
     //The function generates two random radius vector lengths and random cos of angle between them.
-    srand(time(NULL));
     double rP = std::pow(eps * (rand()%(N+1)), 1.0/3.0);
-    double mu = eps * (rand()%(N+1)) - 1; //mu == cos(rP, rQ)
+    double mu = 2 * eps * (rand()%(N+1)) - 1; //mu == cos(rP, rQ)
     double rQ = std::pow(eps * (rand()%(N+1)), 1.0/3.0);
     return std::make_tuple(rP, mu, rQ);
 }
@@ -42,7 +40,7 @@ double first_estimating(double fun(double arg, double m), double m) {
         DoubleTuple foo = roulette();
         sum += fun(rho(foo), m);
     }
-    return std::pow(4.0/3.0, 2) * eps * sum;
+    return 16.0 / 9.0 / N * sum;
 }
 
 double second_estimating(double func(double arg, double m), double m) {
@@ -55,10 +53,11 @@ double second_estimating(double func(double arg, double m), double m) {
         double l = rP*mu + std::sqrt(1 - std::pow(rP, 2)*(1 - std::pow(mu, 2)));
         sum += std::pow(rho_i, 2) * fun(rho_i, m) * l;
     }
-    return 16.0 * eps / 3.0 * sum;
+    return 16.0 / 3.0 / N  * sum;
 }
 
 int main() {
+    srand(time(NULL));
     std::cout << "Integrand:\t 1 / pi^2 * 1 / rho^m\n" <<
     "m = 1\n" << "1st estimating:\t" << accuracy(first_estimating(fun, 1)) << std::endl <<
     "2nd estimating:\t" << accuracy(second_estimating(fun, 1)) << std::endl <<
